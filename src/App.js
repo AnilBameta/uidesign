13760af96c68d79bb712194d5e75a92bf1852c41
AnilBameta <anilbameta@gmail.com>
2022-02-22 20:35:32 +0530

    Small card part done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
+import Component6 from './component6/Component6';
 import Component1 from './components/component1/Component1';
 import Component2 from './components/component2/Component2';
 import Component3 from './components/component3/Component3';
+import Component4 from './components/component4/Component4';
+import Component5 from './components/component5/Component5';
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
     <Component1 />
     <Component2 />
     <Component3 />
+    <Component4 />
+    <Component5 />
+    <Component6 />
     </div>
   );
 }
--- src/components/component3/Component3.js
+++ src/components/component3/Component3.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import styles from './component3.module.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faDesktop} from '@fortawesome/free-solid-svg-icons';
+import {faPuzzlePiece} from '@fortawesome/free-solid-svg-icons';
+import {faHandsHelping} from '@fortawesome/free-solid-svg-icons';
 const Component3 = () => {
     return (
         <div className={styles.container}>
@@ -10,9 +13,48 @@ const Component3 = () => {
             </div>
      
      <div className={styles.cardpart}>
-            <div className={styles.card1}>1</div>
-            <div className={styles.card2}>2</div>
-            <div className={styles.card3}>3</div>
+            <div className={styles.card1}>
+                <div className={styles.cardheading}>
+                  <div className={styles.cardheadingbg}>
+                  <FontAwesomeIcon icon={faDesktop} className={styles.cardsearchicon}></FontAwesomeIcon>
+                  </div>
+                </div>
+                <div className={styles.cardcontent}>
+                   <h3>Responsive & Multipurpose</h3>
+                   <p>dwhew wdwdwq hquhw wdw dhwudh dhwq hdhwqdwh whdwqwh hwhwdwddh dwdwdww</p>
+                </div>
+                <div className={styles.cardbutton}>
+                   <button>Read More</button>
+                </div>
+            </div>
+            <div className={styles.card2}>
+            <div className={styles.cardheading}>
+                  <div className={styles.cardheadingbg}>
+                  <FontAwesomeIcon icon={faPuzzlePiece} className={styles.cardsearchicon}></FontAwesomeIcon>
+                  </div>
+                </div>
+                <div className={styles.cardcontent}>
+                   <h3>Responsive & Multipurpose</h3>
+                   <p>dwhew wdwdwq hquhw wdw dhwudh dhwq hdhwqdwh whdwqwh hwhwdwddh dwdwdww</p>
+                </div>
+                <div className={styles.cardbutton}>
+                   <button>Read More</button>
+                </div>
+            </div>
+            <div className={styles.card3}>
+            <div className={styles.cardheading}>
+                  <div className={styles.cardheadingbg}>
+                  <FontAwesomeIcon icon={faHandsHelping} className={styles.cardsearchicon}></FontAwesomeIcon>
+                  </div>
+                </div>
+                <div className={styles.cardcontent}>
+                   <h3>Responsive & Multipurpose</h3>
+                   <p>dwhew wdwdwq hquhw wdw dhwudh dhwq hdhwqdwh whdwqwh hwhwdwddh dwdwdww</p>
+                </div>
+                <div className={styles.cardbutton}>
+                   <button>Read More</button>
+                </div>
+            </div>
         </div>
         </div>
         
