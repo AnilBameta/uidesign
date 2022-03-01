d8c2409d583a23ed35f150ccf13f1eaa548abdd7
AnilBameta <anilbameta@gmail.com>
2022-03-01 20:53:47 +0530

    Responsive done

--- src/components/component2/Component2.js
+++ src/components/component2/Component2.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './component2.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faSearch} from '@fortawesome/free-solid-svg-icons';
+import {faBars} from '@fortawesome/free-solid-svg-icons';
 const Component2 = () => {
+
+    const [barValue,setBarValue] = useState(false);
+
+    const barFunc = () => {
+        setBarValue(!barValue)
+    }
+
     const navButton = ['Home','Portfolio','Blog','Pages','Features','Mega Menu','Contact']
 return(
+    <>
     <div className={styles.container}>
      <div className={styles.left}>
          <div className={styles.logo}>
@@ -15,6 +24,7 @@ return(
        </div>
      </div>
      <div className={styles.right}>
+     
       {
           navButton.map(navItems => (
               <button className={styles.navbtn}>{navItems}</button>
@@ -22,7 +32,21 @@ return(
       }
       <FontAwesomeIcon icon={faSearch} className={styles.searchicon}></FontAwesomeIcon>
      </div>
+     <FontAwesomeIcon icon={faBars} className={styles.baricon} onClick={barFunc}></FontAwesomeIcon>
+     
     </div>
+    {
+        (barValue) && 
+            <div className={styles.barDropDown}>
+                 {
+             navButton.map(navItems => (
+                 <button className={styles.navbtn}>{navItems}</button>
+             ))
+         }
+   
+                </div>
+        }
+        </>
 )
 }
 export default Component2;
